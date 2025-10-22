d84ea95cae3723db1ecffbfdfb7e47d257413b47
nicolasservidio <nicolasservidio.dm@gmail.com>
2025-10-21 23:36:57 -0300

    Footer update

--- src/views/layout/footer.php
+++ src/views/layout/footer.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/../../../config/constants.php';
 
 <footer class="footer mt-auto py-3 bg-light border-top">
     <div class="container text-center small text-muted">
-        <span>&copy; <?= date('Y') ?> MyBlog. All rights reserved.</span>
+        <span>&copy; <?= date('Y') ?> CMS Blog. All rights reserved.</span>
         <span class="mx-2">|</span>
         <a href="<?= BASE_PATH ?>index.php" class="text-decoration-none">Home</a>
         <span class="mx-2">|</span>
@@ -15,5 +15,7 @@ require_once __DIR__ . '/../../../config/constants.php';
         <a href="<?= BASE_PATH ?>index.php?page=categories-index" class="text-decoration-none">Categories</a>
         <span class="mx-2">|</span>
         <a href="<?= BASE_PATH ?>index.php?page=users-login" class="text-decoration-none">Login</a>
+        <br>
+        <span>Developed by Nicolás Servidio.</span>
     </div>
 </footer>
